af72bf7c67a47ad7231a49566e26884e90e2dd96
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2025-08-12 01:07:40 +0700

    Refactor code using MapStruct Library

--- src/main/java/com/hust/usermanagement/service/UserService.java
+++ src/main/java/com/hust/usermanagement/service/UserService.java
@@ -2,6 +2,7 @@ package com.hust.usermanagement.service;
 
 import com.hust.usermanagement.dto.UserDto;
 import com.hust.usermanagement.entity.User;
+import com.hust.usermanagement.mapper.AutoUserMapper;
 import com.hust.usermanagement.mapper.UserMapper;
 import com.hust.usermanagement.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,23 +25,29 @@ public class UserService {
 
     public UserDto createUser(UserDto user){
 //        User user1 = UserMapper.mapToUser(user);
-        User user1 = modelMapper.map(user, User.class);
+//        User user1 = modelMapper.map(user, User.class);
+        User user1 = AutoUserMapper.MAPPER.mapToUser(user);
+
         User savedUser = userRepository.save(user1);
+
 //        UserDto user2 = UserMapper.mapToUserDto(savedUser);
-        UserDto user2 = modelMapper.map(savedUser, UserDto.class);
+//        UserDto user2 = modelMapper.map(savedUser, UserDto.class);
+        UserDto user2 = AutoUserMapper.MAPPER.mapToUserDto(savedUser);
         return user2;
     }
 
     public UserDto getUserById(Long id){
         User user = userRepository.findById(id).get();
 //        return UserMapper.mapToUserDto(user);
-        return modelMapper.map(user, UserDto.class);
+//        return modelMapper.map(user, UserDto.class);
+        return AutoUserMapper.MAPPER.mapToUserDto(user);
     }
 
     public List<UserDto> getAllUsers(){
         List<User> users = userRepository.findAll();
 //        return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
-        return users.stream().map(user -> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
+//        return users.stream().map(user -> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
+        return users.stream().map(user -> AutoUserMapper.MAPPER.mapToUserDto(user)).collect(Collectors.toList());
     }
 
     public UserDto updateUser(Long id, UserDto user){
@@ -50,7 +56,8 @@ public class UserService {
         existUser.setLastName(user.getLastName());
         existUser.setEmail(user.getEmail());
 //        return UserMapper.mapToUserDto(userRepository.save(existUser));
-        return modelMapper.map(userRepository.save(existUser), UserDto.class);
+//        return modelMapper.map(userRepository.save(existUser), UserDto.class);
+        return AutoUserMapper.MAPPER.mapToUserDto(userRepository.save(existUser));
     }
 
     public void deleteUser(Long id){
